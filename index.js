133e28ab378dd2f048787cf215b6cc40d1d01aa2
guleixibian <guleixibian@outlook.com>
2022-05-05 09:00:19 +0800

    Create basic server

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello World!")
+var http = require("http")
+
+http.createServer(function (request, responce){
+    responce.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    responce.end('<h1>Hello World</h1><p>Welcome to my server. Yay, this is working!</p>');
+}).listen(8888);
+
+console.log("Server running at http://127.0.0.1:8888/")
